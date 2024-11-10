60bf53ad26e09fc74a0c649b73f3e14ccb1d2455
ikeda-99 <k.ikeda.cingroup2012@gmail.com>
2024-11-10 20:01:17 +0900

    MethodPractice.java コードの記述

--- src/basicSyntax/MethodPractice.java
+++ src/basicSyntax/MethodPractice.java
@@ -1,35 +1,48 @@
 package basicSyntax ;
 
 public class MethodPractice {
-	
+
 	public static void main (String[] args) {
-		System.out.println("計算結果は{" + sumNumbers(10,2) + "}です。");
-		System.out.println("計算結果は{" + subNumbers(10,2) + "}です。");
-		System.out.println("計算結果は{" + mulNumbers(10,2) + "}です。");
-		System.out.println("計算結果は{" + divNumbers(10,2) + "}です。");
-	}
-	
-	public static int sumNumbers(int a , int b) {
-		int sum = a + b ;
 		
-		return sum;
-	}
-	
-	public static int subNumbers(int a , int b) {
-		int sum = a - b ;
+		int a = 10;
+		int b = 2;
 		
-		return sum ;
-	}
-	
-	public static int mulNumbers(int a , int b) {
-		int sum = a * b ;
+		int sumResult = sumNumbers(a, b);
+		int subResult = subNumbers(a, b);
+		int mulResult = mulNumbers(a, b);
+		int divResult = divNumbers(a, b);
 		
-		return sum ;
+		results(sumResult);
+        results(subResult);
+        results(mulResult);
+        results(divResult);
 	}
-	
-	public static int divNumbers(int a , int b) {
-		int sum = a / b ;
+
+		public static int sumNumbers(int a , int b) {
+			int sum = a + b ;
+
+			return sum;
+		}
+
+		public static int subNumbers(int a , int b) {
+			int different = a - b ;
+
+			return different ;
+		}
+
+		public static int mulNumbers(int a , int b) {
+			int product = a * b ;
+
+			return product ;
+		}
+
+		public static int divNumbers(int a , int b) {
+			int quotient = a / b ;
+
+			return quotient ;
+		}
 		
-		return sum ;
-	}
+		public static void results(int result) {
+			System.out.println("計算結果は{" + result + "}です。");
+		}
 }
